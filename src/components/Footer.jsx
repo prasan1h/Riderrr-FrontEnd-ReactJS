a28d8f26a5e99f556b7a43398553f876cac90a3e
prasan1h <prasanna.k.h.070@gmail.com>
2026-04-03 13:48:28 +0530

    nav footer updated

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,66 +1,68 @@
 import React from 'react'
 import logo from '../assets/logo/logo_white.png'
+import {Camera} from 'lucide-react'
+import { Link } from 'react-router'
 
 const Footer = () => {
   return (
     <>
-        <footer class="bg-white">
-        <section class="bg-secondary py-10">
-            <article class="bg-white max-w-6xl mx-auto rounded-[30px] flex flex-col py-10 ">
-                <div class="text-center text-secondary text-5xl font-semibold py-3">
+        <footer className="bg-white">
+        <section className="bg-secondary py-10">
+            <article className="bg-white max-w-6xl mx-auto rounded-[30px] flex flex-col py-10 ">
+                <div className="text-center text-secondary text-5xl font-semibold py-3">
                     <p>Ready for Ride ?</p>
                 </div>
-                <div class="text-center text-secondary text-md pb-6">
+                <div className="text-center text-secondary text-md pb-6">
                     <p>Whether you are upgrading or selling, we make it simple.</p>
                 </div>
-                <div class="flex flex-row justify-center items-center gap-5">
-                    <a href="/buy.html"
-                        class="border-2 border-secondary rounded-full bg-white text-secondary px-7 py-2 text-xl text-center align-middle font-bold">Buy
-                        a Bike</a>
-                    <a href="/sell.html"
-                        class="border-2 border-primary bg-secondary text-white rounded-full px-7 py-2 text-xl text-center align-middle font-bold">Sell
-                        Your Bike</a>
+                <div className="flex flex-row justify-center items-center gap-5">
+                    <Link to="/buy"
+                        className="border-2 border-secondary rounded-full bg-white text-secondary px-7 py-2 text-xl text-center align-middle font-bold">Buy
+                        a Bike</Link>
+                    <Link to="/sell"
+                        className="border-2 border-primary bg-secondary text-white rounded-full px-7 py-2 text-xl text-center align-middle font-bold">Sell
+                        Your Bike</Link>
                 </div>
             </article>
             <article>
-                <div class="flex flex-col max-w-6xl mx-auto gap-5 py-10">
-                    <div class="flex flex-row justify-center">
-                        <div class="w-1/3"><img src={logo} alt="logo" width="200px"/></div>
-                        <div class="w-1/3 text-white text-2xl font-bold pl-20">
+                <div className="flex flex-col max-w-6xl mx-auto gap-5 py-10">
+                    <div className="flex flex-row justify-center">
+                        <div className="w-1/3"><img src={logo} alt="logo" width="200px"/></div>
+                        <div className="w-1/3 text-white text-2xl font-bold pl-20">
                             <p>Quick Links</p>
                         </div>
-                        <div class="w-1/3 text-white text-2xl font-bold pl-20">
+                        <div className="w-1/3 text-white text-2xl font-bold pl-20">
                             <p>Reach Us</p>
                         </div>
                     </div>
-                    <div class="flex flex-row justify-center">
-                        <div class="w-1/3 text-white">
+                    <div className="flex flex-row justify-center">
+                        <div className="w-1/3 text-white">
                             <p>Trusted by thousands of riders in Bengaluru.
                                 The safest way to buy and sell used two-wheelers.</p>
                         </div>
-                        <div class="w-1/3 text-white pl-20">
-                            <ul class="flex flex-col gap-3">
-                                <li><a href="/about.html">About Us</a></li>
-                                <li><a href="/contact.html">Contact Us</a></li>
-                                <li><a href="#">Privacy Policies</a></li>
+                        <div className="w-1/3 text-white pl-20">
+                            <ul className="flex flex-col gap-3">
+                                <li><Link to="/about">About Us</Link></li>
+                                <li><Link to="/contact">Contact Us</Link></li>
+                                <li><Link to="#">Privacy Policies</Link></li>
                             </ul>
                         </div>
-                        <div class="w-1/3 text-white flex flex-col gap-5  pl-20">
-                            <div class="flex flex-col ">
+                        <div className="w-1/3 text-white flex flex-col gap-5  pl-20">
+                            <div className="flex flex-col ">
                                 <p>Jalahalli, Bengaluru, KA 560003</p>
                                 <p>support@reride.com</p>
                             </div>
-                            <div class="flex gap-8">
-                                <a href="https://www.facebook.com/" target="_blank"><i data-lucide="facebook"></i></a>
-                                <a href="https://www.instagram.com/?hl=en" target="_blank"><i data-lucide="instagram"></i></a>
-                                <a href="https://x.com/" target="_blank"><i data-lucide="twitter"></i></a>
+                            <div className="flex gap-8">
+                                <Link to="https://www.facebook.com/" target="_blank"><Camera /></Link>
+                                <Link to="https://www.instagram.com/?hl=en" target="_blank"><Camera /></Link>
+                                <Link to="https://x.com/" target="_blank"><Camera /></Link>
                             </div>
                         </div>
                     </div>
                 </div>
             </article>
         </section>
-        <div class="bg-secondary flex justify-center text-white text-center pb-[35px]">
+        <div className="bg-secondary flex justify-center text-white text-center pb-[35px]">
             <p> &copy; 2026 ReRide Bikes. design and developed by thincnext</p>
         </div>
     </footer>
--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,26 +1,27 @@
 import React from 'react'
 import logo from '../assets/logo/Reride_Logo.png'
+import {Link} from 'react-router'
 
 const Nav = () => {
   return (
     <>
-        <header class="p-5 bg-primary">
-        <nav class="flex justify-between justify-self-center items-center w-full px-20">
-            <div class="flex justify-center items-center">
+        <header className="p-5 bg-primary">
+        <nav className="flex justify-between justify-self-center items-center w-full px-20">
+            <div className="flex justify-center items-center">
                 <img src={logo} alt="logo" width="200px"/>
             </div>
-            <div class="">
-                <ul class="flex ">
-                    <li class="m-1"><a href="/home.html"
-                            class="mx-1 p-2 hover:rounded-lg hover:border-b-4 hover:border-b-secondary">Home</a></li>
-                    <li class="m-1"><a href="/buy.html"
-                            class="mx-1 p-2 hover:rounded-lg hover:border-b-4 hover:border-b-secondary">Buy</a></li>
-                    <li class="m-1"><a href="/sell.html"
-                            class="mx-1 p-2 hover:rounded-lg hover:border-b-4 hover:border-b-secondary">Sell</a></li>
-                    <li class="m-1"><a href="/about.html"
-                            class="mx-1 p-2 hover:rounded-lg hover:border-b-4 hover:border-b-secondary">About</a></li>
-                    <li class="m-1"><a href="/contact.html"
-                            class="mx-1 p-2 hover:rounded-lg hover:border-b-4 hover:border-b-secondary">Contact</a></li>
+            <div className="">
+                <ul className="flex ">
+                    <li className="m-1"><Link to="/"
+                            className="mx-1 p-2 hover:rounded-lg hover:border-b-4 hover:border-b-secondary">Home</Link></li>
+                    <li className="m-1"><Link to="/buy"
+                            className="mx-1 p-2 hover:rounded-lg hover:border-b-4 hover:border-b-secondary">Buy</Link></li>
+                    <li className="m-1"><Link to="/sell"
+                            className="mx-1 p-2 hover:rounded-lg hover:border-b-4 hover:border-b-secondary">Sell</Link></li>
+                    <li className="m-1"><Link to="/about"
+                            className="mx-1 p-2 hover:rounded-lg hover:border-b-4 hover:border-b-secondary">About</Link></li>
+                    <li className="m-1"><Link to="/contact"
+                            className="mx-1 p-2 hover:rounded-lg hover:border-b-4 hover:border-b-secondary">Contact</Link></li>
                 </ul>
             </div>
         </nav>
