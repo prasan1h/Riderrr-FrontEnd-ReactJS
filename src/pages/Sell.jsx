49e343d8dd0389058627c06df589680896663357
2004Vishnu <vishnureddykoppad@gmail.com>
2026-04-03 16:41:22 +0530

    added components to about page

--- src/pages/Sell.jsx
+++ src/pages/Sell.jsx
@@ -1,11 +1,15 @@
 import React from 'react'
 import Nav from '../components/Nav'
 import Footer from '../components/Footer'
+import SellBanner from '../components/sell/SellBanner'
+import StepCounter from '../components/sell/StepCounter'
 
 const Sell = () => {
   return (
     <>
     <Nav/>
+    <SellBanner/>
+    <StepCounter/>
     <Footer/>
     </>
   )
