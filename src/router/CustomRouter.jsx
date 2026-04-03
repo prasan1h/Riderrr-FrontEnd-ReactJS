c65b25adc60fce1f88a915888720f43280e68aba
prasan1h <prasanna.k.h.070@gmail.com>
2026-04-03 12:54:38 +0530

    pages created

--- src/router/CustomRouter.jsx
+++ src/router/CustomRouter.jsx
@@ -1,11 +1,19 @@
 import React from 'react'
 import { Route, Routes } from 'react-router'
 import Home from '../pages/Home'
+import Buy from '../pages/Buy'
+import Sell from '../pages/Sell'
+import About from '../pages/About'
+import Contact from '../pages/Contact'
 
 const CustomRouter = () => {
   return (
     <Routes>
         <Route path='/' element={<Home/>}></Route>
+        <Route path='/' element={<Buy/>}></Route>
+        <Route path='/' element={<Sell/>}></Route>
+        <Route path='/' element={<About/>}></Route>
+        <Route path='/' element={<Contact/>}></Route>
     </Routes>
   )
 }
